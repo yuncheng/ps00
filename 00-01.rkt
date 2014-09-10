7bdd7719a352a27a2a0b51c802b0cdb1ac57dc14
yuncheng <macbook@macbookdemacbook-pro.local>
2014-09-10 10:32:11 -0400

    Nested vs Racket arithmetic for EX 1

--- 00-01.rkt
+++ 00-01.rkt
@@ -3,7 +3,8 @@
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname 00-01) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
 ; EX 1
 ; Write an expression whose value s the number of seconds in a leap year (a leap year has 366 days). Next, write 2 more expressions that have the same value.
-
+; Nested arithmetic
 (* (* (* 366 24) 60) 60) ; 24 hours/day, 60 min/hr, 60 sec/min
 (* (* 366 24) 3600)
-(* (* 366 24) (* 60 60))
+; Racket arithmetic
+(* 366 24 60 60)
